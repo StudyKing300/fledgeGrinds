d065fbc5a972e570a67c5f1aa8d7be8497ec2828
StudyKing300 <121196226+StudyKing300@users.noreply.github.com>
2022-12-22 23:23:17 +0000

    Update 💪_Our_Services.py

--- pages/💪_Our_Services.py
+++ pages/💪_Our_Services.py
@@ -14,6 +14,13 @@ def load_lottieurl(url):
         return None
     return r.json()
 
+# Use local CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("style.css")
 
 
 # ---- LOAD ASSETS ----
